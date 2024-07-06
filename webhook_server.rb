451c033c824bf5c1abe090ca34b0902e73f098fe
Catalin Perticas <perticas_catalin@yahoo.com>
2024-07-06 13:57:09 +0300

    refactor, comments and start analyze payload

--- webhook_server.rb
+++ webhook_server.rb
@@ -1,21 +1,32 @@
 require 'sinatra'
 require 'json'
+require 'net/http'
+require 'uri'
 require 'openssl'
 require 'pry'
 
 set :port, 5000
 
-file = File.read("./secrets.json")
-secrets = JSON.parse(file)
+file = File.read("./config.json")
+config = JSON.parse(file)
 
-SECRET = secrets["webhook_secret"]
+SECRET = config["webhook_secret"]
 
 helpers do
+  # Verifies the signature of a payload body against a given signature.
+  #
+  # @param payload_body [String] The body of the payload to be verified.
+  # @param signature [String] The signature to compare against.
+  # @return [Boolean] Returns true if the signature is valid, false otherwise.
   def verify_signature(payload_body, signature)
     return false if signature.nil?
 
     sha1 = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), SECRET, payload_body)
     Rack::Utils.secure_compare(sha1, signature)
+  end
+
+  def analyze_payload(push)
+    binding.pry
   end
 end
 
@@ -26,6 +37,8 @@ post '/webhook' do
 
   push = JSON.parse(payload_body)
   puts "Received payload: #{push.inspect}"
+
+  analyze_payload(push)
 
   'Webhook received'
 end
