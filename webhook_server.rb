908f6364f1f75f9a7482666044c2e3431466f04d
Catalin Perticas <perticas_catalin@yahoo.com>
2024-07-08 15:57:18 +0300

    all good

--- webhook_server.rb
+++ webhook_server.rb
@@ -49,10 +49,10 @@ helpers do
       start_position = patch.lines.first.split(/[^\d]/).select{|s| !s.empty?}.first.to_i
       # However, there can be several hunks in a file
       num_hunks = patch.lines.select{|l| l.start_with?("@@")}.count
-      # Need to remove end of file markers
+      # Need to remove end of file marker
       num_eof = patch.lines.select{|l| l.start_with?("\\ No newline at end of file")}.count
       # This will place the comment at the end of the last hunk)
-      position = patch.lines.count - 2 * num_eof - 1
+      position = patch.lines.count - num_eof - 1
 
       # Request body
       request.body = {
