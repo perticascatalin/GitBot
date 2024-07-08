5780961bad2e10ed01be5c0c28521cdfc498b518
Catalin Perticas <perticas_catalin@yahoo.com>
2024-07-08 15:49:03 +0300

    retry

--- webhook_server.rb
+++ webhook_server.rb
@@ -51,8 +51,8 @@ helpers do
       num_hunks = patch.lines.select{|l| l.start_with?("@@")}.count
       # Need to remove end of file markers
       num_eof = patch.lines.select{|l| l.start_with?("\\ No newline at end of file")}.count
-      # This will place the comment at the end of the last hunk (since hunk markers also count as lines)
-      position = patch.lines.count - num_eof - 1
+      # This will place the comment at the end of the last hunk
+      position = patch.lines.count - num_hunks - num_eof
 
       # Request body
       request.body = {
@@ -64,7 +64,6 @@ helpers do
 
       # Send the request
       response = http.request(request)
-      binding.pry
 
       # Check and print the response
       if response.code.to_i == 201
