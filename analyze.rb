5d7b022e3c5c23b5f6f909c7e26b5a25680274d5
Catalin Perticas <perticas_catalin@yahoo.com>
2024-08-13 16:06:33 +0300

    guard

--- analyze.rb
+++ analyze.rb
@@ -12,21 +12,26 @@ PAGES = config["pages_of_reviews"]
 for pg in 1..PAGES do
 	file = File.read("./data/#{pg}.json")
 	data = JSON.parse(file)
-	for cmt in 0..data.size-1
-		user = data[cmt]["user"]["login"]
-		if user_count[user].nil?
-			user_count[user] = 1
-		else
-			user_count[user] += 1
+	if data.class == Array
+		for cmt in 0..data.size-1
+			user = data[cmt]["user"]["login"]
+			if user_count[user].nil?
+				user_count[user] = 1
+			else
+				user_count[user] += 1
+			end
+			fl = data[cmt]["path"]
+			if file_count[fl].nil?
+				file_count[fl] = 1
+			else
+				file_count[fl] += 1
+			end
 		end
-		fl = data[cmt]["path"]
-		if file_count[fl].nil?
-			file_count[fl] = 1
-		else
-			file_count[fl] += 1
-		end
+	else
+		puts pg
 	end
 end
 
 user_count = user_count.sort_by {|key, value| -value}.to_h
 file_count = file_count.sort_by {|key, value| -value}.to_h
+binding.pry
