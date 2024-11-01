caa31566a25d82fdea3aa91b6bb6dda17e9f9412
Catalin Perticas <perticas_catalin@yahoo.com>
2024-11-01 20:04:30 +0200

    move

--- repo_utils/api_test.rb
+++ repo_utils/api_test.rb
@@ -4,7 +4,7 @@ require 'pry'
 require 'json'
 
 
-uri = URI.parse("https://api.github.com/repos/ruby/ruby/pulls/comments?per_page=2&page=1&sort='created_at'&direction='desc'")
+uri = URI.parse("https://api.github.com/repos/ruby/ruby/pulls/comments?per_page=10&page=1&sort='created_at'&direction='desc'")
 request = Net::HTTP::Get.new(uri)
 request["Accept"] = "application/vnd.github+json"
 request["X-Github-Api-Version"] = "2022-11-28"
