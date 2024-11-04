0a9e9db50be297e41db63a31833ec203900539b6
Catalin Perticas <perticas_catalin@yahoo.com>
2024-11-04 22:11:09 +0200

    count reviews by user, file & folder

--- repo_utils/count_reviews.rb
+++ repo_utils/count_reviews.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 user_count = {}
 file_count = {}
+file_user_count = {}
 
 file = File.read('./config.json')
 config = JSON.parse(file)
@@ -26,11 +27,42 @@ for page in 1..num_pages do
       else
         file_count[fl] += 1
       end
+      if file_user_count[fl].nil?
+        file_user_count[fl] = {}
+      end
+      if file_user_count[fl][user].nil?
+        file_user_count[fl][user] = 1
+      else
+        file_user_count[fl][user] += 1
+      end
     end
   else
     puts page
   end
 end
 
-user_count = user_count.sort_by {|key, value| -value}.to_h
-file_count = file_count.sort_by {|key, value| -value}.to_h
+user_count = user_count.sort_by { |key, value| -value }.to_h
+file_count = file_count.sort_by { |key, value| -value }.to_h
+
+file_user_count = file_user_count
+  .sort_by { |file, users| -users.values.sum }
+  .map do |file, users|
+    [file, users.sort_by { |user, count| -count }.to_h]
+  end
+  .to_h
+
+file_reviews = file_user_count
+aggregated_reviews = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+file_reviews.each do |path, users|
+  folders = path.split('/')
+  folders.each_index do |i|
+    folder_path = folders[0..i].join('/') + '/'
+    users.each do |user, count|
+      aggregated_reviews[folder_path][user] += count
+    end
+  end
+end
+
+sorted_aggregated_reviews = aggregated_reviews.transform_values do |user_reviews|
+  user_reviews.sort_by { |user, count| -count }.to_h
+end
