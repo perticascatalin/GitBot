6b46edba1aa1cecde324f7379f2f096020451110
Catalin Perticas <perticas_catalin@yahoo.com>
2024-11-15 16:31:26 +0200

    rename, add examples to readme

--- README.md
+++ README.md
@@ -24,3 +24,27 @@ Setup steps:
   - "repo_owner" - repository owner
   - "repo_name" - repository name
   - "github_token" - needed if the repository is private
+
+```ruby
+require_relative 'repo_utils/github_helper'
+
+start_page = 1
+end_page = 10
+pull_number = 100
+github_helper = GitHubHelper.new('config.json')
+github_helper.download_comments(start_page, end_page)
+github_helper.download_pull_request(pull_number)
+```
+
+### Repo Analysis
+
+```ruby
+repo_analysis = RepoAnalysis.new('data/reviews')
+repo_analysis.view_page_reviews(1)
+repo_analysis.view_file_reviews('app/lib/x.rb')
+analysis = repo_analysis.analyze_reviews
+analysis[:user_count]
+analysis[:file_count]
+analysis[:file_user_count]
+analysis[:aggregated_reviews]
+```
--- main.rb
+++ main.rb
@@ -9,27 +9,9 @@ require 'date'
 require_relative 'repo_utils/github_helper'
 require_relative 'repo_utils/view_reviews'
 
-# start_page = 178
-# end_page = 178
-# pull_number = 45333
-# github_helper = GitHubHelper.new('config.json')
-# github_helper.download_comments(start_page, end_page)
-# github_helper.download_pull_request(pull_number)
-
-repo_analysis = RepoAnalysis.new('data/reviews')
-# repo_analysis.view_page_reviews(1)
-# repo_analysis.view_file_reviews('app/lib/tournaments2/scoring/engine.rb')
-# analysis = repo_analysis.analyze_reviews
-# analysis[:user_count]
-# analysis[:file_count]
-# analysis[:file_user_count]
-# analysis[:aggregated_reviews]
-binding.pry
-
-
 pull_number = 45333
-# config = JSON.parse(File.read('config.json'))
-# pull_request = JSON.parse(File.read("data/pull_requests/#{pull_number}.json"))
+config = JSON.parse(File.read('config.json'))
+pull_request = JSON.parse(File.read("data/pull_requests/#{pull_number}.json"))
 
 # # Sample usage
 # filtered_data = pull_request.map { |item| item.slice('filename', 'patch') }
