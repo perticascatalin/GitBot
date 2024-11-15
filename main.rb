45a7af738851009fce43018046687fa31c38297a
Catalin Perticas <perticas_catalin@yahoo.com>
2024-11-15 16:23:09 +0200

    refactor and cleanup

--- main.rb
+++ main.rb
@@ -1,13 +1,63 @@
 # main.rb
+require 'awesome_print'
 require 'net/http'
 require 'uri'
 require 'json'
 require 'pry'
+require 'date'
 
 require_relative 'repo_utils/github_helper'
+require_relative 'repo_utils/view_reviews'
 
-start_page = 178
-end_page = 178
-github_helper = GitHubHelper.new('config.json')
-github_helper.download_comments(start_page, end_page)
-github_helper.download_pull_request(45333)
+# start_page = 178
+# end_page = 178
+# pull_number = 45333
+# github_helper = GitHubHelper.new('config.json')
+# github_helper.download_comments(start_page, end_page)
+# github_helper.download_pull_request(pull_number)
+
+repo_analysis = RepoAnalysis.new('data/reviews')
+# repo_analysis.view_page_reviews(1)
+# repo_analysis.view_file_reviews('app/lib/tournaments2/scoring/engine.rb')
+# analysis = repo_analysis.analyze_reviews
+# analysis[:user_count]
+# analysis[:file_count]
+# analysis[:file_user_count]
+# analysis[:aggregated_reviews]
+binding.pry
+
+
+pull_number = 45333
+# config = JSON.parse(File.read('config.json'))
+# pull_request = JSON.parse(File.read("data/pull_requests/#{pull_number}.json"))
+
+# # Sample usage
+# filtered_data = pull_request.map { |item| item.slice('filename', 'patch') }
+
+# # Build the prompt for the whole diff
+# prompt = "Make a summary in natural language of the following code changes:\n\n"
+# filtered_data.each do |entry|
+#   prompt += "File: #{entry['filename']}\nPatch:\n#{entry['patch']}\n\n"
+# end
+
+# # Or just for one file
+# prompt = "Review the following file:\n\n"
+# prompt += filtered_data.find{ |d| d["filename"] == "app/views/divisions/index/_by_multiple_criteria.html.haml"}["patch"]
+
+# # Build the request
+# api_key = config['OPENAI_API_KEY']
+# uri = URI('https://api.openai.com/v1/chat/completions')
+# headers = {
+#   "Content-Type" => "application/json",
+#   "Authorization" => "Bearer #{api_key}"
+# }
+# body = {
+#   model: "gpt-4o",
+#   messages: [{ role: "user", content: prompt }],
+#   max_tokens: 200
+# }
+
+# response = Net::HTTP.post(uri, body.to_json, headers)
+
+# puts "Response from ChatGPT:"
+# puts JSON.parse(response.body)
--- repo_utils/view_reviews.rb
+++ repo_utils/view_reviews.rb
@@ -1,43 +1,111 @@
-require 'json'
-require 'pry'
-require 'date'
-require 'awesome_print'
-
-def print_cmt_data(cmt_data)
-  puts cmt_data['path']
-  puts '===================================='
-  ap cmt_data['body']
-  puts '===================================='
-  puts cmt_data['user']['login']
-  puts DateTime.parse(cmt_data['created_at']).to_date
-  puts "\n\n\n"
-end
-
-def view_page_reviews(page)
-  file = File.read("../data/#{page}.json")
-  data = JSON.parse(file)
-
-  for cmt in 0..data.size-1
-    print_cmt_data(data[cmt])
+class RepoAnalysis
+  def initialize(data_directory)
+    @data_directory = data_directory
   end
-end
 
-def view_file_reviews(file_path)
-  config_file = File.read('../config.json')
-  config = JSON.parse(config_file)
-  num_pages = config['pages_of_reviews']
-
-  for page in 1..num_pages do
-    file = File.read("../data/#{page}.json")
+  def view_page_reviews(page)
+    file = read_data_file(page)
     data = JSON.parse(file)
-    for cmt in 0..data.size-1
-      print_cmt_data(data[cmt]) if data[cmt]['path'] == file_path
+
+    data.each do |cmt|
+      print_cmt_data(cmt)
+    end
+  end
+
+  def view_file_reviews(file_path)
+    num_pages = total_pages
+
+    (1..num_pages).each do |page|
+      file = read_data_file(page)
+      data = JSON.parse(file)
+      data.each do |cmt|
+        print_cmt_data(cmt) if cmt['path'] == file_path
+      end
+    end
+  end
+
+  def analyze_reviews
+    user_count = Hash.new(0)
+    file_count = Hash.new(0)
+    file_user_count = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+
+    total_pages.times do |page|
+      page += 1 # pages start from 1
+      file = read_data_file(page)
+      data = JSON.parse(file)
+
+      if data.is_a?(Array)
+        data.each do |cmt|
+          user = cmt['user']['login']
+          user_count[user] += 1
+
+          fl = cmt['path']
+          file_count[fl] += 1
+
+          file_user_count[fl][user] += 1
+        end
+      else
+        puts "Invalid data format on page #{page}"
+      end
+    end
+
+    sorted_user_count = user_count.sort_by { |_, value| -value }.to_h
+    sorted_file_count = file_count.sort_by { |_, value| -value }.to_h
+
+    file_user_count_sorted = file_user_count
+      .sort_by { |_, users| -users.values.sum }
+      .map { |file, users| [file, users.sort_by { |_, count| -count }.to_h] }
+      .to_h
+
+    aggregated_reviews = aggregate_reviews(file_user_count_sorted)
+
+    {
+      user_count: sorted_user_count,
+      file_count: sorted_file_count,
+      file_user_count: file_user_count_sorted,
+      aggregated_reviews: aggregated_reviews
+    }
+  end
+
+  private
+
+  def total_pages
+    Dir.glob(File.join(@data_directory, '*.json')).map do |file|
+      File.basename(file, '.json').to_i
+    end.max
+  end
+
+  def read_data_file(page)
+    file_path = File.join(@data_directory, "#{page}.json")
+    raise "File not found: #{file_path}" unless File.exist?(file_path)
+
+    File.read(file_path)
+  end
+
+  def print_cmt_data(cmt_data)
+    puts cmt_data['path']
+    puts '===================================='
+    ap cmt_data['body']
+    puts '===================================='
+    puts cmt_data['user']['login']
+    puts DateTime.parse(cmt_data['created_at']).to_date
+    puts "\n\n\n"
+  end
+
+  def aggregate_reviews(file_reviews)
+    aggregated_reviews = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    file_reviews.each do |path, users|
+      folders = path.split('/')
+      folders.each_index do |i|
+        folder_path = folders[0..i].join('/') + '/'
+        users.each do |user, count|
+          aggregated_reviews[folder_path][user] += count
+        end
+      end
+    end
+
+    aggregated_reviews.transform_values do |user_reviews|
+      user_reviews.sort_by { |_, count| -count }.to_h
     end
   end
 end
-
-# page = ARGV[0] # page of reviews to look at
-# view_page_reviews(page)
-
-# file_path = ARGV[0] # file for which to find reviews
-# view_file_reviews(file_path)
